c48eec60680c9d597b7a0d1d816097973556b562
Sharmila Jesupaul <tosharmila@gmail.com>
2017-01-01 16:58:14 -0800

    adds pry

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:google_oauth2]
-	validates :first_name, presence: true
-	validates :last_name, presence: true
+    :recoverable, :rememberable, :trackable, :validatable,
+    :omniauthable, :omniauth_providers => [:google_oauth2]
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+      binding.pry
+      user = User.create(first_name: data["first_name"],
+        last_name: data["last_name"],
+        email: data["email"],
+        password: Devise.friendly_token[0,20]
+      )
+    end
+    user
+  end
 end
