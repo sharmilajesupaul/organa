2db4f28040a07619b1abff0b15ab8b55b3d69da8
Mark Reid <mark.francis.reid@gmail.com>
2017-01-01 17:09:55 -0800

    removes comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(:email => data["email"]).first
-    # Uncomment the section below if you want users to be created if they don't exist
     unless user
       user = User.create(first_name: data["first_name"],
         last_name: data["last_name"],
