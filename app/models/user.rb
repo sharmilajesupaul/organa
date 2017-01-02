7d42e5261eb2303e11418162d914eb1877994fb7
Mark Reid <mark.francis.reid@gmail.com>
2017-01-01 16:51:46 -0800

    adds from_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,19 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:google_oauth2]
 	validates :first_name, presence: true
 	validates :last_name, presence: true
+
+	def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+      user = User.create(first_name: data["first_name"],
+        last_name: data["last_name"],
+        email: data["email"],
+        password: Devise.friendly_token[0,20]
+      )
+    end
+    user
+	end
 end
