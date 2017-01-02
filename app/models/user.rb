1e93402d6965d116c631f795afa5d7e5548837d3
Sharmila Jesupaul <tosharmila@gmail.com>
2017-01-01 16:27:30 -0800

    auth

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:google_oauth2]
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+end
